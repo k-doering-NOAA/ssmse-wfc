0a2df40ed1a95ea63f0de53f6f689a951c070b41
kathryn.doering <kathryn.doering@noaa.gov>
2021-08-27 15:15:06 -0400

    create a west cost figure, also

--- code/3_plot_map.R
+++ code/3_plot_map.R
@@ -7,6 +7,7 @@ library(ggplot2)
 library(ggmap)
 library(maps)
 library(mapdata)
+library(nmfspalette)
 
 usa_dat <- map_data("usa") # we already did this, but we can do it again
 ggplot() + 
@@ -15,5 +16,11 @@ ggplot() +
   coord_fixed(xlim = c(-96, -78),  ylim = c(25, 35), ratio = 1.3)+
   theme_nothing()
 ggsave("figures/GOM_outline.png")
-  
 
+ggplot() + 
+  geom_polygon(data = usa_dat, aes(x=long, y = lat, group = group), 
+               fill = nmfspalette::nmfs_palette("oceans")(2)[2]) + 
+  coord_fixed(xlim = c(-125, -118),  ylim = c(32 , 49), ratio = 1.3)+
+  theme_nothing()
+
+ggsave("figures/Pacific_outline.png")
